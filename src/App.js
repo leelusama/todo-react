1fa12f4b4c0f3922dc764a716368e2cd8b4996c9
Leel Usama <leelusama@gmail.com>
2020-05-27 15:22:30 +0500

    add effect app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Todo from './Todo';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Todo React';
+  }, []);
+
   return (
     <React.Fragment>
       <h1>Todo</h1>
