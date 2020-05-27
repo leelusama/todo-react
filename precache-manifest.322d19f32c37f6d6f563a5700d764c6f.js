a26800cc69a3395c4b1d941c23eea606591d0b95
Leel Usama <leelusama@gmail.com>
2020-05-27 13:38:04 +0500

    Updates

---
+++ precache-manifest.322d19f32c37f6d6f563a5700d764c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97f45bfb21819cefd4cbb2b19b73266",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "4a5ed4f26bd58ef9c51a",
+    "url": "/todo-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f62737e0e1d87a9907e9",
+    "url": "/todo-react/static/js/2.a7da994f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.a7da994f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5ed4f26bd58ef9c51a",
+    "url": "/todo-react/static/js/main.e5f2c32e.chunk.js"
+  },
+  {
+    "revision": "b46e4067e07bd0dd5cbc",
+    "url": "/todo-react/static/js/runtime-main.1c5a09b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
